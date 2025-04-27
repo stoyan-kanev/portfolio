91782c018f5007c49032422ae76dc535ec251cb6
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-04-28 00:27:57 +0300

    Added logo and style changes

--- front-end/src/components/HeroSection.jsx
+++ front-end/src/components/HeroSection.jsx
@@ -5,34 +5,39 @@ function HeroSection() {
     return (
         <section id="home" className="hero">
             <div className="hero-me">
+                <img src="/1647349180831.jpg" className='hidden-me-img' alt="me"/>
+
                 <div>
                     <h1 className="hero-title">Hi, I'm Stoyan</h1>
                     <p className="hero-subtitle">Junior Web Developer</p>
                     <a href="#projects" className="hero-button">View Projects</a>
 
                 </div>
-                <img src="/1647349180831.jpg" alt="me"/>
+                <div className="hero-me-wrap-links">
+                    <img src="/1647349180831.jpg" alt="me"/>
 
-                <div className="contact-items">
+                    <div className="contact-items">
 
-                    <div className="contact-item">
-                        <FaLinkedin className="contact-icon"/>
-                        <a href="https://www.linkedin.com/in/stoyan-kanev-042386207" target="_blank"
-                           rel="noopener noreferrer">
-                            Stoyan Kanev
-                        </a>
-                    </div>
-                    <div className="contact-item">
-                        <FaGithub className="contact-icon"/>
-                        <a href="https://github.com/LazyPotato02" target="_blank" rel="noopener noreferrer">
-                            Stoyan Kanev
-                        </a>
-                    </div>
-                    <div className="contact-item">
-                        <FaEnvelope className="contact-icon"/>
-                        <a href="mailto:stoyan.kanev178@gmail.com">stoyan.kanev178@gmail.com</a>
+                        <div className="contact-item">
+                            <FaLinkedin className="contact-icon"/>
+                            <a href="https://www.linkedin.com/in/stoyan-kanev-042386207" target="_blank"
+                               rel="noopener noreferrer">
+                                Stoyan Kanev
+                            </a>
+                        </div>
+                        <div className="contact-item">
+                            <FaGithub className="contact-icon"/>
+                            <a href="https://github.com/LazyPotato02" target="_blank" rel="noopener noreferrer">
+                                Stoyan Kanev
+                            </a>
+                        </div>
+                        <div className="contact-item">
+                            <FaEnvelope className="contact-icon"/>
+                            <a href="mailto:stoyan.kanev178@gmail.com">stoyan.kanev178@gmail.com</a>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </section>
     );
