d83a280d9d73720bf2a07570c323b9fc5b9b64da
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-04-27 17:03:53 +0300

    Added about

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,11 +5,13 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Certificates from "./components/Certificates.jsx";
+import About from "./components/About.jsx";
 
 function App() {
     return (
         <>
             <HeroSection/>
+            <About/>
             <TechStack/>
             <Certificates/>
             <Projects/>
---
+++ front-end/src/components/About.jsx
@@ -0,0 +1,17 @@
+
+function About() {
+    return (
+        <section className="about">
+            <div className="about-container">
+                <h2>About Me</h2>
+                <p>I'm a motivated and ambitious Junior Web Developer, passionate about learning new technologies and
+                    continuously improving my skills. My goal is to grow and build a long-term career in the IT field,
+                    creating modern, scalable, and efficient applications. I enjoy solving challenges, working on
+                    real-world projects, and collaborating with teams where innovation and growth are encouraged.</p>
+            </div>
+        </section>
+
+    );
+}
+
+export default About;
